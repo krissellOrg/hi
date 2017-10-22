fc957687d1e8156de7dc37e2cf036b4db43d31e6
krissell <krissell@us.ibm.com>
2017-10-22 16:29:26 -0400

    initfial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app = express();
+
+var port = process.env.PORT || 8070;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.send('Hey, you made it. Hello!')
+})
+
+app.listen(port);
+console.log("Listening on port ", port);
